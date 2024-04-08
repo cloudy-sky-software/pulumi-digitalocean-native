6ee330b8d8b3dbad7ac3d98ca8d8dfa0c17b14f1
Praneet Loke <1466314+praneetloke@users.noreply.github.com>
2024-04-07 19:41:34 -0700

    Update the simple example

--- examples/simple/index.ts
+++ examples/simple/index.ts
@@ -1,15 +1,1 @@
-import * as render from "@css/render";
-
-import * as o from "@css/render/types/output";
-
-const service = new render.services.Service("my-service", {
-  name: "My test service",
-  ownerId: "",
-  repo: "https://github.com/praneetloke/test",
-  type: "static_site",
-  serviceDetails: {},
-});
-
-export const staticSiteUrl = service.serviceDetails.apply(
-  (d) => (d as o.services.ServiceStaticSite).url
-);
+import * as digitalocean from "@cloudyskysoftware/pulumi-digitalocean-native";
