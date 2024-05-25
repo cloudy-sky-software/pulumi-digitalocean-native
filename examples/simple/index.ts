16ce2ca29b984b96865a90925410cb2aa720a22b
Praneet Loke <1466314+praneetloke@users.noreply.github.com>
2024-05-25 09:40:13 -0700

    Fix provide go:embed directives

--- examples/simple/index.ts
+++ examples/simple/index.ts
@@ -1,1 +1,8 @@
-import * as digitalocean from "@cloudyskysoftware/pulumi-digitalocean-native";
+import * as pulumi from "@pulumi/pulumi";
+import { droplets } from "@cloudyskysoftware/pulumi-digitalocean-native";
+
+const droplet = new droplets.v2.Droplets("my-vm", {
+  names: ["my-vm"],
+  image: "ubuntu-20-04-x64",
+  size: "s-1vcpu-1gb",
+});
