b911acb310e7eaa3b47f9230cebfe343f8b5361b
Praneet Loke <1466314+praneetloke@users.noreply.github.com>
2024-05-26 13:11:24 -0700

    Update example

--- examples/simple/index.ts
+++ examples/simple/index.ts
@@ -1,8 +1,8 @@
 import * as pulumi from "@pulumi/pulumi";
 import { droplets } from "@cloudyskysoftware/pulumi-digitalocean-native";
 
-const droplet = new droplets.v2.Droplets("my-vm", {
-  names: ["my-vm"],
+const droplet = new droplets.v2.Droplet("my-vm", {
+  name: "my-vm",
   image: "ubuntu-20-04-x64",
   size: "s-1vcpu-1gb",
 });
